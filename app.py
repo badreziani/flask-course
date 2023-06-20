12248672c523756ea364f2229c047d1398ed535d
Badre ZIANI <badreziani@gmail.com>
2023-06-20 11:57:17 +0100

    Update

--- app.py
+++ app.py
@@ -10,30 +10,43 @@ def get_stores():
     return {"stores": list(stores.values())}
 
 
-@app.post("/store")
-def create_store():
-    data = request.get_json()
-    store_id = uuid.uuid4().hex
-    new_store = {"id": store_id, **data}
-    stores[store_id] = new_store
-    print(new_store)
-    return new_store, 201
-
-
 @app.get("/store/<string:store_id>")
 def get_store(store_id: str):
     try:
         return stores[store_id]
     except KeyError:
-        return {"details": "Store not found"}, 404
+        abort(404, message="Store not found.")
+
+
+@app.post("/store")
+def create_store():
+    data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {"id": store_id, **data}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.get("/item/<string:item_id>")
+def get_stores(item_id: str):
+    try:
+        return stores[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.post("/item")
+def create_item():
+    data = request.get_json()
+    if data["store_id"] not in stores:
+        abort(404, message="Store not found.")
+
+    item_id = uuid.uuid4().hex
+    item = {"id": item_id, **data}
+    items[item_id] = item
+    return item, 201
 
 
 @app.get("/item")
-def get_store_items():
-    data = request.get_json()
-    if data["store_id"] not in stores:
-        return {"details": "Store not found"}, 404
-    item_id = uuid.uuid4().hex
-    new_item = {"id": item_id, **data}
-    items[item_id] = new_item
-    return new_item, 201
+def get_items():
+    return {"items": list(items.values())}
